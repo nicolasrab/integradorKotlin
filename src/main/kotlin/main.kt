47ad71822557b5341227aad462f9277c14343842
nicolasrab3 <nicolas.rabellino@mercadolibre.com>
2021-09-12 19:57:39 -0300

    uncommment parkedTime

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -9,11 +9,11 @@ const val PLACES_AVAILABLES = 20
 
 data class Parkable(var vehicle: Vehicle) {
     var plate = vehicle.plate
-//    val parkedTime: Long
-//        get() = (Calendar.getInstance().timeInMillis - vehicle.checkInTime.timeInMillis) / MINUTES_IN_MILISECONDS
+    val parkedTime: Long
+        get() = (Calendar.getInstance().timeInMillis - vehicle.checkInTime.timeInMillis) / MINUTES_IN_MILISECONDS
 
-    val parkedTime = 198.toLong()
-    fun calculateFee() : Int{
+
+    fun calculateFee(): Int {
         return applyDiscount(calculatePricePerTime())
     }
 
@@ -23,12 +23,11 @@ data class Parkable(var vehicle: Vehicle) {
         return calculatePricePerTime
     }
 
-    private fun calculatePricePerTime() : Int {
-        var amount : Int = 0
-        if (parkedTime <= 120){
+    private fun calculatePricePerTime(): Int {
+        var amount: Int = 0
+        if (parkedTime <= 120) {
             amount = vehicle.type.value
-        }
-        else {
+        } else {
             var extraMin = parkedTime - 120
             amount = vehicle.type.value + calculateExtraTimeCost(extraMin)
         }
@@ -57,7 +56,7 @@ data class Parkable(var vehicle: Vehicle) {
 data class Parking(val parkables: MutableSet<Parkable>) {
 
     var emptyPlaces = PLACES_AVAILABLES - parkables.size
-    var retiredVehicles : Pair<Int,Int> = Pair(0,0)
+    var retiredVehicles: Pair<Int, Int> = Pair(0, 0)
 
     fun checkIn(vehicle: Vehicle) {
         if (emptyPlaces != 0 && addVehicle(vehicle)) {
@@ -71,11 +70,12 @@ data class Parking(val parkables: MutableSet<Parkable>) {
             return parkables.add(Parkable(vehicle))
         return false
     }
+
     fun checkOutVehicle(plate: String, onSuccess: (Int) -> Unit, onError: () -> Unit) {
         val parkable = parkables.firstOrNull {
             it.plate == plate
         }
-        if (parkable == null){
+        if (parkable == null) {
             onError()
         } else {
             val fee = parkable.calculateFee()
@@ -87,15 +87,15 @@ data class Parking(val parkables: MutableSet<Parkable>) {
     }
 
     private fun updateRetiredVehicles(fee: Int) {
-            retiredVehicles=retiredVehicles.copy(first = retiredVehicles.first+1)
-            retiredVehicles=retiredVehicles.copy(second = retiredVehicles.second+fee)
+        retiredVehicles = retiredVehicles.copy(first = retiredVehicles.first + 1)
+        retiredVehicles = retiredVehicles.copy(second = retiredVehicles.second + fee)
     }
 
-    fun showRetiredVehiclesInformation(){
+    fun showRetiredVehiclesInformation() {
         println("${retiredVehicles.first} vehicles have checked out and have earnings of ${retiredVehicles.second}")
     }
 
-    fun listParking(){
+    fun listParking() {
         parkables.map {
             println(it.plate)
         }
@@ -130,57 +130,64 @@ enum class VehicleType(val value: Int) {
 
 fun main() {
     val parking = Parking(mutableSetOf())
-    //prueba cargar 20 sabiendo que hay 4 cargados, deberia fallar 4 veces
 
+    val car = Vehicle("AA111AA", VehicleType.CAR, Calendar.getInstance(), "DISCOUNT_CARD_001")
+    val moto = Vehicle("BB222BB", VehicleType.MOTORCYCLE, Calendar.getInstance())
+    val minibus = Vehicle("CC333CC", VehicleType.MINIBUS, Calendar.getInstance(), "DISCOUNT_CARD_002")
+    val bus = Vehicle("DD444DD", VehicleType.BUS, Calendar.getInstance())
 
-    val car = Vehicle("AA111AA",VehicleType.CAR, Calendar.getInstance(),"DISCOUNT_CARD_001")
-    val moto = Vehicle("BB222BB",VehicleType.MOTORCYCLE, Calendar.getInstance())
-    val minibus = Vehicle("CC333CC",VehicleType.MINIBUS, Calendar.getInstance(),"DISCOUNT_CARD_002")
-    val bus = Vehicle("DD444DD",VehicleType.BUS, Calendar.getInstance())
+    val car2 = Vehicle("AA111AA", VehicleType.CAR, Calendar.getInstance(), "DISCOUNT_CARD_003")
+    val moto2 = Vehicle("EE555EE", VehicleType.MOTORCYCLE, Calendar.getInstance())
+    val minibus2 = Vehicle("FF666FF", VehicleType.MINIBUS, Calendar.getInstance(), "DISCOUNT_CARD_004")
+    val bus2 = Vehicle("GG777GG", VehicleType.BUS, Calendar.getInstance())
 
-    val car2 = Vehicle("AA111AA",VehicleType.CAR, Calendar.getInstance(),"DISCOUNT_CARD_003")
-    val moto2 = Vehicle("EE555EE",VehicleType.MOTORCYCLE, Calendar.getInstance())
-    val minibus2 = Vehicle("FF666FF",VehicleType.MINIBUS, Calendar.getInstance(),"DISCOUNT_CARD_004")
-    val bus2 = Vehicle("GG777GG",VehicleType.BUS, Calendar.getInstance())
+    val car3 = Vehicle("HH888HH", VehicleType.CAR, Calendar.getInstance())
+    val moto3 = Vehicle("II999II", VehicleType.MOTORCYCLE, Calendar.getInstance())
+    val minibus3 = Vehicle("JJ000JJ", VehicleType.MINIBUS, Calendar.getInstance(), "DISCOUNT_CARD_006")
+    val bus3 = Vehicle("KK100KK", VehicleType.BUS, Calendar.getInstance())
 
-    val car3 = Vehicle("HH888HH",VehicleType.CAR, Calendar.getInstance())
-    val moto3 = Vehicle("II999II",VehicleType.MOTORCYCLE, Calendar.getInstance())
-    val minibus3 = Vehicle("JJ000JJ",VehicleType.MINIBUS, Calendar.getInstance(),"DISCOUNT_CARD_006")
-    val bus3 = Vehicle("KK100KK",VehicleType.BUS, Calendar.getInstance())
+    val car4 = Vehicle("LL200LL", VehicleType.CAR, Calendar.getInstance(), "DISCOUNT_CARD_007")
+    val moto4 = Vehicle("MM300MM", VehicleType.MOTORCYCLE, Calendar.getInstance())
+    val minibus4 = Vehicle("NN400NN", VehicleType.MINIBUS, Calendar.getInstance(), "DISCOUNT_CARD_008")
+    val bus4 = Vehicle("OO500OO", VehicleType.BUS, Calendar.getInstance())
 
-    val car4 = Vehicle("LL200LL",VehicleType.CAR, Calendar.getInstance(),"DISCOUNT_CARD_007")
-    val moto4 = Vehicle("MM300MM",VehicleType.MOTORCYCLE, Calendar.getInstance())
-    val minibus4 = Vehicle("NN400NN",VehicleType.MINIBUS, Calendar.getInstance(),"DISCOUNT_CARD_008")
-    val bus4 = Vehicle("OO500OO",VehicleType.BUS, Calendar.getInstance())
+    val car5 = Vehicle("PP600PP", VehicleType.CAR, Calendar.getInstance(), "DISCOUNT_CARD_009")
+    val moto5 = Vehicle("QQ700QQ", VehicleType.MOTORCYCLE, Calendar.getInstance())
+    val minibus5 = Vehicle("RR800RR", VehicleType.MINIBUS, Calendar.getInstance(), "DISCOUNT_CARD_010")
+    val bus5 = Vehicle("SS900SS", VehicleType.BUS, Calendar.getInstance())
 
-    val car5 = Vehicle("PP600PP",VehicleType.CAR, Calendar.getInstance(),"DISCOUNT_CARD_009")
-    val moto5 = Vehicle("QQ700QQ",VehicleType.MOTORCYCLE, Calendar.getInstance())
-    val minibus5 = Vehicle("RR800RR",VehicleType.MINIBUS, Calendar.getInstance(),"DISCOUNT_CARD_010")
-    val bus5 = Vehicle("SS900SS",VehicleType.BUS, Calendar.getInstance())
-
-    val car6 = Vehicle("TT010TT",VehicleType.CAR, Calendar.getInstance(),"DISCOUNT_CARD_011")
-    val moto6 = Vehicle("UU020UU",VehicleType.MOTORCYCLE, Calendar.getInstance())
-    val minibus6 = Vehicle("VV030VV",VehicleType.MINIBUS, Calendar.getInstance(),"DISCOUNT_CARD_012")
-    val bus6 = Vehicle("WW040WW",VehicleType.BUS, Calendar.getInstance())
+    val car6 = Vehicle("TT010TT", VehicleType.CAR, Calendar.getInstance(), "DISCOUNT_CARD_011")
+    val moto6 = Vehicle("UU020UU", VehicleType.MOTORCYCLE, Calendar.getInstance())
+    val minibus6 = Vehicle("VV030VV", VehicleType.MINIBUS, Calendar.getInstance(), "DISCOUNT_CARD_012")
+    val bus6 = Vehicle("WW040WW", VehicleType.BUS, Calendar.getInstance())
 
     val listVehicles = listOf(
         car, car2, car3, car4, car5, car6,
         moto, moto2, moto3, moto4, moto5, moto6,
         minibus, minibus2, minibus3, minibus4, minibus5, minibus6,
-        bus, bus2, bus3, bus4, bus5, bus6)
+        bus, bus2, bus3, bus4, bus5, bus6
+    )
 
     //for (num in 1..30) {
-      //  parking.checkIn(Vehicle("AA1${num}1AA", VehicleType.CAR, Calendar.getInstance(), "DISCOUNT_CARD_001"))
+    //  parking.checkIn(Vehicle("AA1${num}1AA", VehicleType.CAR, Calendar.getInstance(), "DISCOUNT_CARD_001"))
     //}
     listVehicles.map {
         parking.checkIn(it)
     }
 
 
-    parking.checkOutVehicle("HH888HH",{println("Your fee is $it. Come back soon.")},{ println("Sorry, the check-out failed")})
-    parking.checkOutVehicle("RR800RR",{println("Your fee is $it. Come back soon.")},{ println("Sorry, the check-out failed")})
-    parking.checkOutVehicle("TT010TT",{println("Your fee is $it. Come back soon.")},{ println("Sorry, the check-out failed")})
-    parking.checkOutVehicle("TT010TT",{println("Your fee is $it. Come back soon.")},{ println("Sorry, the check-out failed")})
+    parking.checkOutVehicle("HH888HH",
+        { println("Your fee is $it. Come back soon.") },
+        { println("Sorry, the check-out failed") })
+    parking.checkOutVehicle("RR800RR",
+        { println("Your fee is $it. Come back soon.") },
+        { println("Sorry, the check-out failed") })
+    parking.checkOutVehicle("TT010TT",
+        { println("Your fee is $it. Come back soon.") },
+        { println("Sorry, the check-out failed") })
+    parking.checkOutVehicle("TT010TT",
+        { println("Your fee is $it. Come back soon.") },
+        { println("Sorry, the check-out failed") })
 
     parking.listParking()
 
